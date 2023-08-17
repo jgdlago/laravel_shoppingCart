f7ce67c1c0c1ed51473ba18dcbb4278ae1afd954
João Dal Lago <joaogabrieldallago@hotmail.com>
2023-08-17 10:08:46 -0300

    Docker config Laravel | Nginx | Postgres | pgadmin

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -352,8 +352,8 @@ class ComposerStaticInitf8932d3c5b75058ff4dc0856bd23d7be
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
